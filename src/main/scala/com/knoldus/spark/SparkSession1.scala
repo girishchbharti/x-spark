514040d15755bacfdd834046739e047a748c070a
girishchbharti <girish@knoldus.com>
2016-07-20 10:55:43 +0530

    AS-01 Refacored app names

--- src/main/scala/com/knoldus/spark/SparkSession1.scala
+++ src/main/scala/com/knoldus/spark/SparkSession1.scala
@@ -9,7 +9,7 @@ object SparkSession1 {
 
   def main(args: Array[String]) {
     val logFile = "/home/knoldus/SPARK/spark-2.0.0-preview-bin-hadoop2.3/README.md" // Should be some file on your system
-    val conf = new SparkConf().setAppName("Assignment1").setMaster("local[4]")
+    val conf = new SparkConf().setAppName("SparkSample").setMaster("local[4]")
     val sc = new SparkContext(conf)
     val logData = sc.textFile(logFile, 2).cache()
     val numAs = logData.filter(line => line.contains("a")).count()
